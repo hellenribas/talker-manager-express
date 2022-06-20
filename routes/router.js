e0216f933205bb84c756e1ea749d3a8fcd21a47e
hellenribas <tchwtrabalho@gmail.com>
2022-06-20 19:52:56 -0300

    feat: Criando rota talker/:id

--- routes/router.js
+++ routes/router.js
@@ -9,6 +9,16 @@ router.get('/', async (_req, res) => {
   res.status(200).json(JSON.parse(talker));
 });
 
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const talker = await readFile('./talker.json');
+  const palestrante = JSON.parse(talker).find((e) => e.id === Number(id));
+  if (!palestrante || palestrante === undefined) {
+    res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  res.status(200).json(palestrante);
+});
+
 module.exports = {
   router,
 };
